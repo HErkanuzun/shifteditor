3720c3fa70422371586b69cdb83cd1894c05f491
ERKAN UZUN <erkanuzun@ERKANs-MacBook-Pro.local>
2022-10-05 15:37:18 +0300

    template import

---
+++ resources/views/layouts/frontbase.blade.php
@@ -0,0 +1,9 @@
+@include('home.sections.header')
+@include('home.sections.sidebar')
+
+@section('content')
+    
+
+@endsection
+
+@include('home.sections.footer')
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home.pages.home');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/about', [HomeController::class, 'about'])->name('about');
+Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
+Route::get('/login', [HomeController::class, 'login'])->name('login');
+Route::get('/logout', [HomeController::class, 'logout'])->name('logout');
+Route::get('/register', [HomeController::class,'register'])->name('register');
+
+
+
+Route::get('/admin', [AdminController::class, 'admin_index'])->name('admin_index');
